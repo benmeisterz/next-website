4a910624d49a871688d25c2e7c3ecdfcc504ba48
benmeisterz <ben.naldoza@gmail.com>
2023-09-09 19:00:35 +0800

    chino

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,22 @@
+import { Roboto, Saira_Condensed } from 'next/font/google'
+import Image from 'next/image'
+
+const roboto = Roboto({ subsets: ['latin'], weight: '300' })
+const sair = Saira_Condensed({ subsets: ['latin'], weight: '300' })
+
 export const Hero = () => {
   return (
-    <div>
-      <h1>hero</h1>
-      <img src='https://unsplash.com/photos/JmVaNyemtN8' alt='' />
-    </div>
+    <header className='padding-x w-full py-[5px] bg-bgdark text-light font-bold text-lg lg:pt-[150px]'>
+      <div className='max-container flex '>
+        <Image
+          src='/images/deftones-22lead.jpg'
+          alt='/'
+          width={970} // Set the width
+          height={700}
+          className='flex flex-end'
+        />
+        <h1>sdfkljklsdjg</h1>
+      </div>
+    </header>
   )
 }
