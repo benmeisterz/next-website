763c8d424fa257187cf4cbedf83c58120ea73025
benmeisterz <ben.naldoza@gmail.com>
2023-09-09 18:41:11 +0800

    added image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import Footer from '@/components/Footer'
 import { Nav } from '@/components/Nav'
+import { Hero } from '@/components/Hero'
 
 export default function Home() {
   return (
     <div>
       {/* section */}
       <Nav />
-
+      <Hero />
       <Footer />
     </div>
   )
